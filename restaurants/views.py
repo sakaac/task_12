8fb4c217a9e717745f35c9089ed095b2c7e70a11
Shaha Al-Khamees <sakaac@Shahas-MacBook.local>
2019-02-12 16:08:28 +0300

    submission

--- restaurants/views.py
+++ restaurants/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from .models import Restaurant, Item
 from .forms import RestaurantForm, ItemForm, SignupForm, SigninForm
 from django.contrib.auth import login, authenticate, logout
+from django.db.models import Q
 
 def no_access(request):
     return render(request, 'no_access.html')
@@ -46,6 +47,13 @@ def signout(request):
     return redirect("signin")
 
 def restaurant_list(request):
+    restaurant = Restaurant.objects.all()
+    query = request.GET.get('q')
+    if query:
+        restaurants = Restaurant.objects.filter(
+            Q(name__contains=query)|
+            Q(owner__username__contains=query)|
+            Q(description__contains=query))
     context = {
         "restaurants":Restaurant.objects.all()
     }
